3d0bd53fb85f014b604fbedb743ee5d35b81b8e6
mikeg631 <mikeg631@ufl.edu>
2020-02-17 18:34:19 -0500

    here
    stuff

--- Michael/ReadDICOM.cxx
+++ Michael/ReadDICOM.cxx
@@ -4,10 +4,15 @@
 #include <vtkDICOMImageReader.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkOBJExporter.h>
+#include <vtkExporter.h>
 #include <vtkImageData.h>
 #include <vtkRenderer.h>
 #include <vector>
 #include <Windows.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkPolyData.h>
+#include <vtkInteractorStyleTrackballCamera.h>
 
 
 
@@ -62,20 +67,58 @@ int main(int argc, char* argv[])
     imageAppend->Update();
 
     //vtkOBJExporter
+   // vtkSmartPointer<vtkRenderer> renderer;
+    //renderer = vtkRenderer::New();
+    vtkSmartPointer<vtkPolyData> model = vtkPolyData::New();
+    model->DeepCopy(imageAppend->GetOutput());
+    vtkSmartPointer<vtkPolyDataMapper> myDataMapper = vtkPolyDataMapper::New();
+    myDataMapper->SetInputData(model);
+    vtkActor* myActor = vtkActor::New();
+    myActor->SetMapper(myDataMapper);
+
+    /*
+    //assign our actor to the renderer
+    renderer->AddActor(myActor);
+    std::string temp = "test";
+    vtkRenderWindow* renWin = vtkRenderWindow::New();
+    renWin->AddRenderer(renderer);
+    vtkSmartPointer<vtkOBJExporter> objExporter = vtkOBJExporter::New();
+    objExporter->SetRenderWindow(renWin);
+    objExporter->SetFilePrefix(temp.c_str());
+    objExporter->Update();
+    objExporter->Write();*/
 
 
-    // Visualize
-    vtkSmartPointer<vtkImageViewer2> imageViewer =
-        vtkSmartPointer<vtkImageViewer2>::New();
-    imageViewer->SetInputConnection(imageAppend->GetOutputPort());
+    // Create an actor
+
+    // A renderer and render window
+    vtkSmartPointer<vtkRenderer> renderer =
+        vtkSmartPointer<vtkRenderer>::New();
+    vtkSmartPointer<vtkRenderWindow> renderWindow =
+        vtkSmartPointer<vtkRenderWindow>::New();
+    renderWindow->AddRenderer(renderer);
+
+    // An interactor
     vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
         vtkSmartPointer<vtkRenderWindowInteractor>::New();
-    imageViewer->SetupInteractor(renderWindowInteractor);
-    imageViewer->Render();
-    imageViewer->GetRenderer()->ResetCamera();
-    imageViewer->Render();
+    renderWindowInteractor->SetRenderWindow(renderWindow);
 
+    renderer->AddActor(myActor);
+    renderer->SetBackground(1, 1, 1); // Background color white
+
+    // Render
+    renderWindow->Render();
+
+    /*vtkSmartPointer<vtkInteractorStyleTrackballCamera> style =
+        vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New(); //like paraview
+
+    renderWindowInteractor->SetInteractorStyle(style);*/
+
+    // Begin mouse interaction
     renderWindowInteractor->Start();
+
+  
+
 
     return EXIT_SUCCESS;
 }
