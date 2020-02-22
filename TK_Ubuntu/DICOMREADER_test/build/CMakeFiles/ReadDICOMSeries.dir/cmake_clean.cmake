44310f5bb8db26c4c831f5d65ba4e31901f7e0c9
Tikahari <tikaharik@gmail.com>
2020-02-22 11:54:46 -0500

    Dicom reader example

---
+++ TK_Ubuntu/DICOMREADER_test/build/CMakeFiles/ReadDICOMSeries.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReadDICOMSeries.dir/ReadDICOMSeries.cxx.o"
+  "ReadDICOMSeries"
+  "ReadDICOMSeries.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReadDICOMSeries.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
